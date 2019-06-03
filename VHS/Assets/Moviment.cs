c9a4062ade59cc78761cb1a26f50b8de03df7997
Victor Dano <victor.dano99@hotmail.com>
2019-06-03 18:34:42 -0300

    [ADD] Movimentação Esquerda e Direita (Velocidade Ajustável no Inspector)

---
+++ VHS/Assets/Moviment.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Moviment : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+    
+    [SerializeField]
+     private float velocidade = 0;
+    float horizontal;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        horizontal = Input.GetAxis("Horizontal");
+        
+        Move(horizontal);
+    }
+
+    private void Move(float hor)
+    {
+        rb2D.velocity = new Vector2(hor*velocidade,rb2D.velocity.y);
+    }
+
+}
