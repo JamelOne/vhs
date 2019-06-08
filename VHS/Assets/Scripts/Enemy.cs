9ced1f9508395689d48a5261df8614e9bedd0e6d
Victor Dano <victor.dano99@hotmail.com>
2019-06-08 14:08:51 -0300

    [ADD] Scripts Enemy e Player Attack

--- VHS/Assets/Scripts/Enemy.cs
+++ VHS/Assets/Scripts/Enemy.cs
@@ -4,16 +4,102 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public int health;
-    public float speed;
+    private Rigidbody rb;
+    private Transform groundCheck;
+    private bool onGround;
+    private bool facingRight = true;
+    private Transform target;
+    private bool isDead = false;
+    private float zForce;
+    private float walkTimer;
+    private float currentSpeed;
+    public float maxSpeed;
+    private bool damaged=false;
+    private float damageTime=0.5f;
+    private float damageTimer;
+    private int currentHealth;
+    public float minHeight, maxHeight;
+    public int maxHealth;
+    public float attackRate = 1f;
+    private float nextAttack;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb=GetComponent<Rigidbody>();
+        groundCheck=transform.Find("GroundCheck");
+        target = FindObjectOfType<Moviment>().transform;
+        currentHealth=maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
+         onGround = Physics.Linecast(transform.position, groundCheck.position, 1<<LayerMask.NameToLayer("Ground"));
+        facingRight = (target.position.x < transform.position.x) ? false : true;
+        if(facingRight)
+        {
+            transform.eulerAngles = new Vector3(0,0,0);
+        }else{
+            transform.eulerAngles = new Vector3(0,180,0);
+        }
+        walkTimer += Time.deltaTime;
+        if(damaged && !isDead){
+            damageTimer += Time.deltaTime;
+            if(damageTimer >= damageTime){
+                damaged=false;
+                damageTimer=0;
+            }
+        }
+   }
+
+   private void FixedUpdate(){
+       if(!isDead){
+            Vector3 targetDistance = target.position - transform.position;
+            float hForce=targetDistance.x / Mathf.Abs(targetDistance.x);
+
+        if(walkTimer >= Random.Range(1f,2f))
+        {
+            zForce = Random.Range(-1,2);
+            walkTimer=0;
+        }
+
+        if(Mathf.Abs(targetDistance.x) < 1.5f) 
+        {
+            hForce=0;
+        }
+       
+       if(!damaged){
+           rb.velocity= new Vector3(hForce*currentSpeed,0,zForce*currentSpeed);
+        if(Mathf.Abs(targetDistance.x)<1.5f && Mathf.Abs(targetDistance.z) <1.5f && Time.time > nextAttack){
+            currentSpeed=0;
+            nextAttack=Time.time+attackRate;
+        }
+       }
+
+        rb.velocity = new Vector3(hForce*currentSpeed, 0, zForce*currentSpeed);
+       
+   }
+   rb.position = new Vector3 (rb.position.x,rb.position.y,Mathf.Clamp(rb.position.z,minHeight, maxHeight));
+}
+
+public void TookDamage(int damage){
+    if(!isDead)
+    {
+        damaged = true;
+        currentHealth-=damage;
+        if(currentHealth <=0){
+            isDead=true;
+            rb.AddRelativeForce(new Vector3(3,5,0),ForceMode.Impulse);
+        }
+    }
+}
+
+public void DisableEnemy(){
+    gameObject.SetActive(false);
+}
+    void ResetSpeed()
+    {
+        currentSpeed=maxSpeed;
     }
 }
